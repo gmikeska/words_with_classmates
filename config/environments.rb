7eaeb7c25d95d77fe408a8c211ea7a6ad955df4b
Greg Mikeska <gmikeska07@gmail.com>
2015-02-17 12:04:48 -0600

    db fix for master

--- config/environments.rb
+++ config/environments.rb
@@ -3,5 +3,8 @@ require 'active_record_tasks'
 require_relative '../lib/models.rb' # the path to your application file
 
 
-ActiveRecord::Base.establish_connection('postgres://xnxrqbvaakrrtm:GC-Z1095Zda7FjdMdYAQcvDygF@ec2-107-22-249-138.compute-1.amazonaws.com:5432/d2j9d3fh98p3uu')
-
+ActiveRecord::Base.establish_connection(
+  :adapter => 'postgresql',
+  :host     => 'localhost',
+  :database => 'wordGame'
+)
--- db/migrate/20150209090819_create_game_sessions.rb
+++
@@ -1,7 +0,0 @@
-class CreateGameSessions < ActiveRecord::Migration
-  def change
-    create_table :game_session do |t|
-    	t.string :gameboard
-    end
-  end
-end
