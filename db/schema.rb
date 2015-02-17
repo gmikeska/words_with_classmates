4d6101301b05817a6010952c792b3f2e87d0d428
Greg Mikeska <gmikeska07@gmail.com>
2015-02-17 13:55:04 -0600

    db issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217123115) do
+ActiveRecord::Schema.define(version: 20150217134732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 20150217123115) do
   create_table "user_sessions", force: :cascade do |t|
     t.integer "user_id"
     t.integer "session_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_hash"
   end
 
 end
