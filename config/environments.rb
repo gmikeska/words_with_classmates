b6bca70ab7a6d4e5f5fc15c33c6fd479d2ef0ad6
Greg Mikeska <gmikeska07@gmail.com>
2015-02-17 13:42:48 -0600

    db issues

--- config/environments.rb
+++ config/environments.rb
@@ -2,7 +2,6 @@ require 'active_record'
 require 'active_record_tasks'
 require_relative '../lib/models.rb' # the path to your application file
 
-p ENV['DBHOST']
 ActiveRecord::Base.establish_connection(
   :adapter => 'postgresql',
   :host     => ENV['DBHOST'],
