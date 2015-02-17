831e1ebbb8c42aba7b6ad20f638ba4391c85f90d
Greg Mikeska <gmikeska07@gmail.com>
2015-02-17 11:28:33 -0600

    heroku pg db url

--- config/environments.rb
+++ config/environments.rb
@@ -3,9 +3,5 @@ require 'active_record_tasks'
 require_relative '../lib/models.rb' # the path to your application file
 
 
-ActiveRecord::Base.establish_connection(
-  :adapter => 'postgresql',
-  :host     => 'localhost',
-  :database => 'wordGame'
-)
+ActiveRecord::Base.establish_connection(HEROKU_POSTGRESQL_AQUA_URL)
 
