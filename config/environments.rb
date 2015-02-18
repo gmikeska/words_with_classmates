69c087044b52c5bc0bb2dce57346465bb7302e62
Greg Mikeska <gmikeska07@gmail.com>
2015-02-18 10:10:28 -0600

    db troubleshooting

--- config/environments.rb
+++ config/environments.rb
@@ -1,12 +1,5 @@
 require 'active_record'
-require 'active_record_tasks'
+#require 'active_record_tasks'
 require_relative '../lib/models.rb' # the path to your application file
 
-ActiveRecord::Base.establish_connection(
-  :adapter => 'postgresql',
-  :host     => ENV['DBHOST'],
-  :username => ENV['DBUSR'],
-  :password => ENV['DBPASS'],
-  :database => ENV['DBNAME']
-)
-
+ActiveRecord::Base.establish_connection(:url => ENV['DATABASE_URL'], :adapter => 'postgresql', :database => 'd3i2apnhscbctr')
